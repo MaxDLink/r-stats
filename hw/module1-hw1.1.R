c18d683480df32f821007f26ef693a6990fd05f3
Max Link <66765685+MaxDLink@users.noreply.github.com>
2025-08-31 15:39:34 -0500

    added a simplified margin of error formula from hw1 feedback to check for errors
    MOE_simple = z_star*SE_p_hat
    
    Understood what the 1/4 was in question 6 with one_fourth = .5*.5
    
    Included 1 in the question 7 formula with p* = .2
    
    samplesize2 = (z_star/.07)^2*.2*(1-.2)

--- hw/module1-hw1.1.R
+++ hw/module1-hw1.1.R
@@ -33,3 +33,42 @@ cond2 = n*(1-p_hat)
 print(cond1)
 print(cond2)
 
+# SE of p_hat 
+
+SE_p_hat = sqrt((p_hat*(1-p_hat))/n)
+print(SE_p_hat)
+
+# Critical Value (z_star)
+# what is p value for qnorm()? 
+# Know probability for C.I. is 95%
+one_tail = (1 - .95)/2
+p_zs = .95 + one_tail # why? 
+z_star = qnorm(p_zs)
+
+# MOE 
+MOE_simple = z_star*SE_p_hat
+MOE = z_star*(sqrt((.5*(1-.5))/n))
+
+#Lower & upper bounds of large sample C.I. 
+# RIN is met, so: 
+upper_interval = p_hat + z_star*(sqrt((p_hat*(1 - p_hat)) / n))
+lower_interval = p_hat - z_star*(sqrt((p_hat*(1 - p_hat)) / n))
+interval <- c(lower_interval, upper_interval)
+
+# display to console 
+interval # range of plausible values for the parameter 
+
+# parameter --- The value we are trying to determine 
+# 1,003 adults, 462 used cellphone 
+# true p value between (0.4297709 0.4914654)
+
+# do not know p, use .5 
+# MOE <= .25. Use .25? 
+samplesize = (z_star/.25)^2*.5*.5
+
+one_fourth = .5*.5
+
+# 20% surveyed in previous evals, so p* = .2
+samplesize2 = (z_star/.07)^2*.2*(1-.2)
+
+
